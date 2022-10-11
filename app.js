4e42fcf2ef81ae3a08b8483c4e5a66ec0d3bad15
Rafał Konieczny <rkonieczny87@gmail.com>
2022-10-12 00:25:03 +0200

    tasks added with the use of variables in app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 let pi = 3.1415;
-console.log(2*10*pi)
-console.log(2*5*pi)
-console.log(2*7*pi)
-console.log(2*8*pi)
-console.log(2*9*pi)
+console.log(2*10*pi);
+console.log(2*5*pi);
+console.log(2*7*pi);
+console.log(2*8*pi);
+console.log(2*9*pi);
+
+let C = 299792458;
+console.log(C);
+
+let M = 10;
+let E = M * C^2
+console.log(E)
