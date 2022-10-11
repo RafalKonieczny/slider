7e9ee6b376deb6dc8d665bab4fae4eb95b3c1963
Rafał Konieczny <rkonieczny87@gmail.com>
2022-10-12 01:25:11 +0200

    added variables and objects for dots, arrows and slide in app.js and modified index.html

--- app.js
+++ app.js
@@ -1,33 +1,14 @@
-let name = "Rafał";
-let lastName = "Konieczny";
-console.log(name + " " + lastName);
+let arrowLeft = document.querySelector(".arrow-left");
+console.log(arrowLeft);
+let arrowRight = document.querySelector(".arrow-right");
+console.log(arrowRight);
 
-name = "Jan";
-lastName = "Kowalski";
-console.log(name + " " + lastName);
+let dot1 = document.querySelector("#dot1");
+let dot2 = document.querySelector("#dot2");
+let dot3 = document.querySelector("#dot3");
 
-let person1 = {
-    name: "Rafał",
-    lastName: "Konieczny"
-};
-console.log(person1.name + " " + person1.lastName);
+let slide1 = document.querySelector("#slide1");
+let slide2 = document.querySelector("#slide2");
+let slide3 = document.querySelector("#slide3");
 
-let person2 = {
-    name: "Jan",
-    lastName: "Kowalski"
-};
-console.log(person2.name + " " + person2.lastName);
-
-let car1 = {
-    mark: "Honda",
-    model: "Civic",
-    productionYear: 1995
-}
-console.log(car1.mark + " " + car1.model + " " + " Rok produkcji: " + car1.productionYear)
-
-let car2 = {
-    mark: "Skoda",
-    model: "Kamiq",
-    productionYear: 2020
-}
-console.log(car2.mark + " " + car2.model + " " + " Rok produkcji: " + car2.productionYear)
+dot2.addEventListener("click");
