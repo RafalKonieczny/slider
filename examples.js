ad73278ef837a5cfcf022f5e0b3ea7e3863f3729
Rafał Konieczny <rkonieczny87@gmail.com>
2022-10-12 20:53:25 +0200

    added myFirstFunction and Tasks on functions in examples.js

---
+++ examples.js
@@ -0,0 +1,25 @@
+let myFirstFunction = (name, greeting) => {
+    console.log(greeting + "! Mam na imię " + name + "!");
+};
+
+myFirstFunction("Rafał", "Hej");
+myFirstFunction("Jan", "Dzień dobry");
+
+
+let multiplyBy2 = (number) => {
+    console.log(number*2);
+}
+
+multiplyBy2(2);
+
+let multiply = (a, b) => {
+    console.log(a * b);
+}
+
+multiply(2, 4);
+
+let square = (number) => {
+    console.log(number * number);
+}
+
+square(2);
