336dd87cfc4c1b326cf0154ccfe77a19fbe8277f
Rafał Konieczny <rkonieczny87@gmail.com>
2022-10-12 23:42:30 +0200

    added loop for in examples3.js

---
+++ examples3.js
@@ -0,0 +1,15 @@
+let circleLenght = (radius) => {
+    console.log(2*radius*3.14);
+}
+
+for(let i=1; i<=20; i++) {
+    circleLenght(i);
+}
+
+let nOddNumbers = (n) => {
+    console.log(2*n - 1);
+}
+
+for(let i=1; i<=20; i++) {
+    nOddNumbers(i);
+}
